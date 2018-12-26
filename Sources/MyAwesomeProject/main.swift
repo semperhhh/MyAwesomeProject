c752e1bea0711a238b88d17caa501adc0a68baf3
张鹏辉 <zphui5409@sina.com>
2018-12-26 20:23:01 +0800

    init

---
+++ Sources/MyAwesomeProject/main.swift
@@ -0,0 +1,26 @@
+print("Hello, world!")
+
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+import PerfectMustache
+
+let server = HTTPServer()
+
+server.documentRoot = "./webroot"
+
+var routes = Routes()
+
+routes.add(method: .get, uri: "/") { (_, response) in
+    response.setHeader(HTTPResponseHeader.Name.contentType, value: "text/html")
+    response.setBody(string: "body")
+    response.completed()
+}
+
+do {
+    
+    server.serverPort = 8181
+    try server.start()
+}catch PerfectError.networkError(let err, let msg) {
+    print("Network error thrown: \(err) \(msg)")
+}
